17faf91b63c94f2342206c0ecac9c04043f3777c
Leon <leon@MacBook-Air-de-Mac.local>
2018-09-05 17:28:51 -0500

    First commit

---
+++ sem2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,116 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*var x = "Fuera de func"
+
+func printSth(n:Int){
+    let x = "Hola desde func"
+    print(x)
+    print(n)
+}
+
+print("Constant"+x)
+printSth(n: 3)*/
+
+/*var interpol = "Interpolation"
+
+print("A Variable inside a string is an \(interpol)")*/
+
+
+/*typealias integer = Int
+var x = 3
+var y:integer = 1
+print("Resultado \(x+y)")*/
+
+/*var x = "Bruno"
+var y = "Gerardo"
+if y==x{
+    print("True")
+}else{
+    print("False")
+}*/
+
+/*let x = 1
+let y = 2
+
+
+if (x<y) && (x>0){
+    print("X es menor que \(y) y mayor que 0")
+}*/
+
+/*var nota = 17
+switch nota {
+case 0..<13:
+    print("\(nota) menor a 13")
+    fallthrough
+case 13..<16:
+    print("\(nota) menor a 16")
+    fallthrough
+case 16..<21:
+    print("\(nota) menor a 20")
+default:
+    print("Inválido")
+}*/
+
+/*var msg:String?
+var nro = 10
+
+msg = (nro == 10) ? "El número es diez":"No es diez"
+
+print(msg!)*/
+
+/*func printing(_ cadena:String){
+    print(cadena)
+}
+
+func printing2()->String{
+    return "Soy una función"
+}
+
+print(printing2())
+printing("Soy un método")
+*/
+
+/*var arr : [Int]=[1,2,3,4,5,6]
+
+for i in arr{
+    if i==3{
+        continue
+    }else{
+        print("\(i)")
+    }
+}*/
+
+/*var x = 0
+while x<10{
+    print("\(x)")
+    x+=1
+}*/
+
+/*var x = 0
+
+repeat{
+    print("\(x)")
+    x+=1
+}while x < 5
+*/
+
+/*for k in 1...5{
+    print("Tabla del \(k):")
+    for l in 1...10{
+     print("\(k) * \(l) = \(k*l)")
+    }
+}*/
+
+var dict = ["uno":"Hola","dos":"Mundo"]
+print(dict["uno"])
+
+
+
+
+
+
+
+
